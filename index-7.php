d5b981493a1af67d4d47b89e01a91f244715ef68
Rupesh <S220630769@itsbhm.org>
2022-07-29 06:53:39 +0530

    Datatypes Added

---
+++ index-7.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+String
+Integer
+Float (floating point numbers - also called double)
+Boolean
+Array
+Object
+NULL
+Resource
+*/
+
+$s  = "String";
+$i = 10;
+$f = 10.2;
+$b = true; // false
+$a = array("Volvo","BMW","Toyota");
+$value = NULL;
+
+// Object -> ON HOLD
+// Resource -> ON HOLD
